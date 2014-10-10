9a56f5b90d042c241b9ccf1fb7437e0443513ea4
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-10-10 15:26:00 +0200

    refac: rename channel => canal
    because the channel name is taken in Chicken.

--- canal.scm
+++ canal.scm
@@ -1,44 +1,44 @@
-;; channel.scm -- thread-safe channel (FIFO) library
+;; canal.scm -- thread-safe channel (FIFO) library
 ;; Copyright (c) 2012 Alex Shinn.  All rights reserved.
 ;; BSD-style license: http://synthcode.com/license.txt
 
-(define-record-type Channel
-  (%make-channel mutex condvar front rear)
-  channel?
-  (mutex channel-mutex channel-mutex-set!)
-  (condvar channel-condvar channel-condvar-set!)
-  (front channel-front channel-front-set!)
-  (rear channel-rear channel-rear-set!))
+(define-record-type canal
+  (%make-canal mutex condvar front rear)
+  canal?
+  (mutex canal-mutex canal-mutex-set!)
+  (condvar canal-condvar canal-condvar-set!)
+  (front canal-front canal-front-set!)
+  (rear canal-rear canal-rear-set!))
 
-(define (make-channel)
-  (%make-channel (make-mutex) (make-condition-variable) '() '()))
+(define (make-canal)
+  (%make-canal (make-mutex) (make-condition-variable) '() '()))
 
-(define (channel-empty? chan)
-  (null? (channel-front chan)))
+(define (canal-empty? chan)
+  (null? (canal-front chan)))
 
-(define (channel-send! chan obj)
-  (mutex-lock! (channel-mutex chan))
+(define (canal-send! chan obj)
+  (mutex-lock! (canal-mutex chan))
   (let ((new (list obj))
-        (rear (channel-rear chan)))
-    (channel-rear-set! chan new)
+        (rear (canal-rear chan)))
+    (canal-rear-set! chan new)
     (cond
      ((pair? rear)
       (set-cdr! rear new))
-     (else  ; sending to empty channel
-      (channel-front-set! chan new)
-      (condition-variable-signal! (channel-condvar chan)))))
-  (mutex-unlock! (channel-mutex chan)))
+     (else  ; sending to empty canal
+      (canal-front-set! chan new)
+      (condition-variable-signal! (canal-condvar chan)))))
+  (mutex-unlock! (canal-mutex chan)))
 
-(define (channel-receive! chan)
-  (mutex-lock! (channel-mutex chan))
-  (let ((front (channel-front chan)))
+(define (canal-receive! chan)
+  (mutex-lock! (canal-mutex chan))
+  (let ((front (canal-front chan)))
     (cond
-     ((null? front)  ; receiving from empty channel
-      (mutex-unlock! (channel-mutex chan) (channel-condvar chan))
-      (channel-receive! chan))
+     ((null? front)  ; receiving from empty canal
+      (mutex-unlock! (canal-mutex chan) (canal-condvar chan))
+      (canal-receive! chan))
      (else
-      (channel-front-set! chan (cdr front))
+      (canal-front-set! chan (cdr front))
       (if (null? (cdr front))
-          (channel-rear-set! chan '()))
-      (mutex-unlock! (channel-mutex chan))
+          (canal-rear-set! chan '()))
+      (mutex-unlock! (canal-mutex chan))
       (car front)))))
