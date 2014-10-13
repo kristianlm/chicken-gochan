e983a52793854df88870ddbaaed26f8b2a6ae8b0
Kristian Lein-Mathisen <kristianlein@gmail.com>
2014-10-13 21:04:36 +0200

    feat: adds canal-close and canal-closed?
    it errors on recv from empty and closed canal, which is bad. we can use
    an optional procedure arg, for example, here. It'd be nice to have
    ranges like we do in Go.

--- canal.scm
+++ canal.scm
@@ -3,22 +3,28 @@
 ;; Copyright (c) 2014 Kristian Lein-Mathisen.  All rights reserved.
 ;; BSD-style license: http://synthcode.com/license.txt
 
+(use srfi-18)
+
 (define-record-type canal
-  (%make-canal mutex condvar front rear)
+  (%make-canal mutex condvar front rear closed?)
   canal?
   (mutex canal-mutex canal-mutex-set!)
   (condvar canal-condvar canal-condvar-set!)
   (front canal-front canal-front-set!)
-  (rear canal-rear canal-rear-set!))
+  (rear canal-rear canal-rear-set!)
+  (closed? canal-closed? canal-closed-set!))
 
 (define (make-canal)
-  (%make-canal (make-mutex) (make-condition-variable) '() '()))
+  (%make-canal (make-mutex) (make-condition-variable) '() '() #f))
 
 (define (canal-empty? chan)
   (null? (canal-front chan)))
 
 (define (canal-send chan obj)
   (mutex-lock! (canal-mutex chan))
+  (when (canal-closed? chan)
+    (begin (mutex-unlock! (canal-mutex chan))
+           (error "cannot send to closed canal" chan)))
   (let ((new (list obj))
         (rear (canal-rear chan)))
     (canal-rear-set! chan new)
@@ -35,6 +41,7 @@
   (let ((front (canal-front chan)))
     (cond
      ((null? front) ;; receiving from empty canal
+      (when (canal-closed? chan) (error "cannel is closed" chan))
       (mutex-unlock! (canal-mutex chan) (canal-condvar chan))
       (canal-receive chan))
      (else
@@ -43,3 +50,8 @@
           (canal-rear-set! chan '()))
       (mutex-unlock! (canal-mutex chan))
       (car front)))))
+
+(define (canal-close c)
+  (mutex-lock! (canal-mutex c))
+  (canal-closed-set! c #t)
+  (mutex-unlock! (canal-mutex c)))
---
+++ tests/run.scm
@@ -0,0 +1,18 @@
+
+(include "canal.scm")
+
+(use test)
+(test-group
+ "simple canal"
+ (define c (make-canal))
+ (canal-send c 'one)
+ (test "synchronous recv" 'one (canal-receive c))
+ (canal-send c 'two)
+ (canal-close c)
+ (test "closed?" #t (canal-closed? c))
+ (test-error "send to closed canal fails" (canal-send c 'three))
+
+ (test "buffered closed canal" 'two (canal-receive c) )
+ (test-error "errors on recving from closed and empty canal" (canal-receive c) )
+
+ )
