b9daa78075ea9bd25279f53e6f180628be1213a1
Kristian Lein-Mathisen <kristianlein@gmail.com>
2017-04-03 21:27:46 +0200

    test: adds some basic test cases
    I don't know how useful these are. Ah, well the timeout tests are
    probably quite handy. Glad it's all passing (for now anyway!)

--- tests/run.scm
+++ tests/run.scm
@@ -15,22 +15,75 @@
  (test '(hello #t meta!) (receive (gochan-select* `((,chan meta!))))))
 
 (test-group
+ "unbuffered 2 channels, 1 channel ready"
+ (define chan1 (gochan 0))
+ (define chan2 (gochan 0))
+ (go (gochan-send chan1 'one)
+     (gochan-send chan1 'two))
+
+ (test "pick from data-ready in order data first"
+       'one
+       (gochan-select ((chan1 -> msg) msg)
+                      ((chan2 -> msg) 'wrong!)))
+ (test "pick from data-ready in order data last"
+       'two
+       (gochan-select ((chan2 -> msg) 'wrong!)
+                      ((chan1 -> msg) msg))))
+
+(test-group
+ "unbuffered 2 channels, 2 channels ready"
+ (define chan1 (gochan 0))
+ (define chan2 (gochan 0))
+ (go (gochan-send chan1 1)
+     (gochan-send chan1 2))
+ (go (gochan-send chan2 3)
+     (gochan-send chan2 4))
+
+ (test "all messages received exactly once (order unknown by design)"
+       '(1 2 3 4)
+       (sort
+        (list (gochan-select ((chan1 -> msg) msg)
+                             ((chan2 -> msg) msg))
+              (gochan-select ((chan1 -> msg) msg)
+                             ((chan2 -> msg) msg))
+              (gochan-select ((chan2 -> msg) msg)
+                             ((chan1 -> msg) msg))
+              (gochan-select ((chan2 -> msg) msg)
+                             ((chan1 -> msg) msg)))
+        <)))
+
+(test-group
  "unbuffered 1 channel fifo, primordial first"
 
  (define chan (gochan 0))
- (go (thread-sleep! 0.01)
+ (go (thread-yield!)
      (gochan-send chan 1)
-     (gochan-send chan 2)
-     (test -1 (gochan-recv chan))
-     (test -2 (gochan-recv chan))
-     (gochan-send chan 3)
-     (test -3 (gochan-recv chan)))
- ;;      msg ok  meta
- (test 1 (gochan-recv chan))
- (test 2 (gochan-recv chan))
- (gochan-send chan -1)
- (gochan-send chan -2)
- (test 3 (gochan-recv chan))
- (gochan-send chan -3))
+     (gochan-send chan 2))
+ (test "1 channel fifo priomaridal first" 1 (gochan-recv chan))
+ (test "1 channel fifo priomaridal first" 2 (gochan-recv chan))
 
+ (define chan (gochan 0))
+ (go (gochan-send chan 1)
+     (gochan-send chan 2))
+ (thread-yield!)
+ (test "1 channel fifo goroutine   first" 1 (gochan-recv chan))
+ (test "1 channel fifo goroutine   first" 2 (gochan-recv chan)))
 
+(test-group
+ "timers"
+ (define to1 (gochan-after 100))
+ (define to2 (gochan-after 200))
+ (define reply (gochan 0))
+
+ (go (gochan-select ((to1 -> x) (gochan-send reply 'to1))
+                    ((to2 -> x) (gochan-send reply 'to2))))
+ (go (gochan-select ((to1 -> x) (gochan-send reply 'to1))
+                    ((to2 -> x) (gochan-send reply 'to2))))
+
+ (define start (current-milliseconds))
+ (test "timeout order 1" 'to1 (gochan-recv reply))
+ (test "timeout order 2" 'to2 (gochan-recv reply))
+ (define duration (- (current-milliseconds) start))
+ (test "200ms to timeout took <220ms " #t (begin (print* "(" duration ")")(< duration 220))))
+
+(test-exit)
