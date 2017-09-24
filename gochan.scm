848abc0dcae9094240a3c62aab8232235a9defdb
Kristian Lein-Mathisen <kristianlein@gmail.com>
2017-09-24 21:36:45 +0200

    portability: put chicken-stuff in chicken-module.scm
    define-record-printer isn't any standard as far as I know, neither
    is (use ...)

--- gochan.scm
+++ gochan.scm
@@ -3,12 +3,6 @@
 ;;
 ;; Copyright (c) 2017 Kristian Lein-Mathisen.  All rights reserved.
 ;; License: BSD
-(use srfi-18
-     (only matchable match)
-     (only extras random)
-     (only data-structures list->queue queue->list sort
-           queue-add! queue-empty? queue-remove! queue-length))
-
 
 (define (info . args) (values))
 ;;(define (info . args) (for-each display (cons (current-thread) (cons " " args))) (newline))
@@ -602,25 +596,4 @@
          (gochan-select* (gochan-select-alist form ...))
        (meta msg fail)))))
 
-(define-record-printer gochan
-  (lambda (x p)
-    (display "#<gochan " p)
-    (if (gochan-closed x) (display "closed "))
-    (display (- (queue-length (gochan-senders x))
-                (queue-length (gochan-receivers x)))  p)
-    (display " (" p)
-    (display (queue-length (gochan-buffer x)) p)
-    (display "/" p)
-    (display (gochan-cap x) p)
-    (display ")>" p)))
 
-(define-record-printer gotimer
-  (lambda (x p)
-    (display "#<gochan ⌛ " p)
-    (display (if (gotimer-when x)
-                 (inexact->exact (round (- (gotimer-when x) (current-milliseconds))))
-                 "∞") p)
-    (display "ms (" p)
-    (display (queue-length (gotimer-receivers x)) p)
-    (display ")" p)
-    (display ">" p)))
